28641086f9d3998181bc3851fd4c1f1b6f51f448
Balaican Ovidiu <oviovi9947@yahoo.com>
2020-11-05 16:50:33 +0200

    Updated README with prohect explanation

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+static void Sort(int v[],int n)
+{
+int i,j,k=0;
+	for(i=0;i<n-1-i;i++)
+		if(v[i]>v[i+1])
+		{
+			k=v[i];
+			v[i]=v[i+1];
+			v[i+1]=k;
+		}
+}
+int main()
+{
+	return 0;
+}
